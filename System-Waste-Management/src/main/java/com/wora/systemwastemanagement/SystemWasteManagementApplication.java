d957e72df59ad8406624350c594e634c93f950aa
mohamed <mohamednouaoui5@gmail.com>
2024-11-29 19:35:23 +0100

    pushed

---
+++ System-Waste-Management/src/main/java/com/wora/systemwastemanagement/SystemWasteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wora.systemwastemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemWasteManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemWasteManagementApplication.class, args);
+    }
+
+}
---
+++ System-Waste-Management/src/test/java/com/wora/systemwastemanagement/SystemWasteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.systemwastemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemWasteManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
